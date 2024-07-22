a3a788625291f0851bfc7250da8a9d1ed9bbf9ab
Edward Vong <evusa142@gmail.com>
2024-07-22 00:21:44 -0700

    new TileMap (TM) + proper cam track for TM

--- SpriteNodes/levelScene/followCam.gd
+++ SpriteNodes/levelScene/followCam.gd
@@ -1,9 +1,13 @@
 extends Camera2D
 
-
+@export var tilemap: TileMap
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var map_rect = tilemap.get_used_rect()
+	var tile_size = tilemap.cell_quadrant_size
+	var pixel_world_size = map_rect.size * tile_size
+	limit_right = pixel_world_size.x
+	limit_bottom = pixel_world_size.y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
