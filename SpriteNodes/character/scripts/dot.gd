6f7e72d2a6c8bf9c8a98468eaee2974d3c31a9bc
Edward Vong <evusa142@gmail.com>
2024-07-22 00:37:04 -0700

    minor adjustments pt 2

--- SpriteNodes/character/scripts/dot.gd
+++ SpriteNodes/character/scripts/dot.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
+
 @export var speed = 300 #max character spd (pixel/sec)
 @export var accel = 30  #charater acceleration up to max spd
 
 func _physics_process(_delta: float) -> void:
-	var direction: Vector2 = Input.get_vector("up", "down","left", "right", )
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down",)
 	
 	if Input.is_action_pressed("left"):
 		$Dot.flip_h = true
